bcb8619c09e71022b5cc2355a0ddf89676baea4a
Musab Hussein Mohamed <police49r5a@gmail.com>
2024-08-13 08:36:30 +0300

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,7 +1,6 @@
-
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session');
+const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -9,34 +8,28 @@ const app = express();
 
 app.use(express.json());
 
-app.use(session({
-    secret: "fingerprint_customer",
-    resave: true,
-    saveUninitialized: true
-}));
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer", customer_routes);
-
-app.use("/customer/auth/*", (req, res, next) => {
-    //Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
     if (req.session.authorization) {
-        const token = req.session.authorization['accessToken'];
-
+        token = req.session.authorization['accessToken'];
         jwt.verify(token, "access", (err, user) => {
             if (!err) {
                 req.user = user;
                 next();
-            } else {
-                return res.status(403).json({ message: "User not authenticated" });
+            }
+            else {
+                return res.status(403).json({ message: "User not authenticated" })
             }
         });
     } else {
-        return res.status(403).json({ message: "User not logged in" });
+        return res.status(403).json({ message: "User not logged in" })
     }
 });
 
-app.use("/", genl_routes);
-
 const PORT = 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(PORT, () => console.log("Server is running"));
